b09be88366a0112a5f6513156fafcadcadd4d9b7
Matt Messier <matt@capsule8.com>
2018-01-12 16:56:49 -0500

    Add support for filtering perf events via the expression engine

--- pkg/sensor/sensor.go
+++ pkg/sensor/sensor.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/capsule8/capsule8/pkg/config"
 	"github.com/capsule8/capsule8/pkg/container"
+	"github.com/capsule8/capsule8/pkg/expression"
 	"github.com/capsule8/capsule8/pkg/stream"
 	"github.com/capsule8/capsule8/pkg/sys"
 	"github.com/capsule8/capsule8/pkg/sys/perf"
@@ -178,17 +179,30 @@ func (s *Sensor) Stop() {
 	}
 }
 
-func (s *Sensor) dispatchSample(eventID uint64, sample interface{}, err error) {
-	if err != nil {
-		glog.Warning(err)
+func (s *Sensor) dispatchSample(eventID uint64, sample perf.EventMonitorSample) {
+	if sample.Err != nil {
+		glog.Warning(sample.Err)
 	}
 
-	if event, ok := sample.(*api.TelemetryEvent); ok && event != nil {
+	if event, ok := sample.DecodedSample.(*api.TelemetryEvent); ok && event != nil {
 		eventMap := s.eventMap.getMap()
 		eventMap.forEach(func(eventID, subscriptionID uint64, s *subscription) {
-			if s.data != nil {
-				s.data <- event
+			if s.data == nil {
+				return
 			}
+			if s.filter != nil {
+				v, err := s.filter.Evaluate(
+					expression.FieldTypeMap(sample.Fields),
+					expression.FieldValueMap(sample.DecodedData))
+				if err != nil {
+					glog.V(1).Infof("Expression evaluation error: %s", err)
+					return
+				}
+				if !expression.IsValueTrue(v) {
+					return
+				}
+			}
+			s.data <- event
 		})
 	}
 }
